94913b23acfd31cce529c69160d1afe8ac2e9d30
Kellojo <kellojo1@gmail.com>
2022-12-11 21:56:04 +0100

    update

--- Simple Loot Table/Scripts/GameObjectLootTable.cs
+++ Simple Loot Table/Scripts/GameObjectLootTable.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 using Kellojo.SimpleLootTable;
 using Kellojo.SimpleLootTable.Editor;
 
-[CreateAssetMenu(menuName = "Kellojo/Loot Table/Game Object Loot Table")]
-public class GameObjectLootTable : LootTableBase<GameObject> {}
 
-[System.Serializable]
-public class GameObjectDropConfig : DropConfig<GameObject> { }
+namespace Kellojo.SimpleLootTable {
 
+    [CreateAssetMenu(menuName = "Kellojo/Loot Table/Game Object Loot Table")]
+    public class GameObjectLootTable : LootTableBase<GameObject> {}
+
+    [System.Serializable]
+    public class GameObjectDropConfig : DropConfig<GameObject> { }
+
+}
 
 #if UNITY_EDITOR
 
